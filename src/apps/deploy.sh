cddcdd99887e8335877cfeb7adde3440e46acaf5
Vaclav Sistek <vsistek@fsfe.org>
2020-05-10 03:15:34 +0200

    Make deploy ensure that log dir exists (useful to bootstrap)

--- src/apps/deploy.sh
+++ src/apps/deploy.sh
@@ -3,6 +3,8 @@ export LANG=C.UTF-8
 
 echo "Content-Type: text/plain; charset=utf-8"
 echo
+echo mkdir log
+mkdir -p /var/www/lektorkavpraze-static/log
 cd /var/www/lektorkavpraze-static
 echo git pull
 git pull
