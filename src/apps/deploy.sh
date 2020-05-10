b1018c24e7b51173612c6cc1b2fbe0f46e3b62cd
Vaclav Sistek <vsistek@fsfe.org>
2020-05-10 04:14:25 +0200

    reverse condition

--- src/apps/deploy.sh
+++ src/apps/deploy.sh
@@ -2,9 +2,11 @@
 export LANG=C.UTF-8
 
 LOCKFILE=/var/www/lektorkavpraze-static/lock/just-deployed
+
 echo "Content-Type: text/plain; charset=utf-8"
 echo
-[ `find "$LOCKFILE" -mmin +5 2>/dev/null` ] && rm $LOCKFILE
+find "$LOCKFILE" -mmin +5 2>/dev/null
+[ $? -ne 0 ] && rm $LOCKFILE
 [ -f LOCKFILE ] && echo "deployed within 5min, try later" && exit 0
 echo mkdir log
 mkdir -p /var/www/lektorkavpraze-static/log
