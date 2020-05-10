5ec56d47738f14f6719cf0946a7b03a903501552
Vaclav Sistek <vsistek@fsfe.org>
2020-05-11 00:47:34 +0200

    fix buildmenu

--- src/util/buildmenu.sh
+++ src/util/buildmenu.sh
@@ -8,7 +8,7 @@ for MD in `ls $SRCROOT/*.md`; do
         NAME=`cat $MD | awk 'match($0,/##NAME## .*/) { print substr($0,RSTART+9,RLENGTH-10)}'`
     MENUITEM=`cat $MD | awk 'match($0,/##MENUITEM## .*/) { print substr($0,RSTART+13,RLENGTH-14)}'`
 
-    if [ $MENUITEM != "none" ]; then
+    if [ "$MENUITEM" != "none" ]; then
         LINE="<li><a "
         [ $NAME == $ACTIVE ] && LINE+="class=\"active\" "
         LINE+="href=\"/$NAME.html\">$MENUITEM</a></li>"
