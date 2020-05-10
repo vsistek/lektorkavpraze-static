35153133cb5a9f7f0d383caadce990f8ead97bd6
Vaclav Sistek <vsistek@fsfe.org>
2020-05-10 03:09:09 +0200

    Make deploy also pull git

--- src/apps/deploy.sh
+++ src/apps/deploy.sh
@@ -3,4 +3,8 @@ export LANG=C.UTF-8
 
 echo "Content-Type: text/plain; charset=utf-8"
 echo
+cd /var/www/lektorkavpraze-static
+echo git pull
+git pull
+echo src/util/build.sh
 /var/www/lektorkavpraze-static/src/util/build.sh
