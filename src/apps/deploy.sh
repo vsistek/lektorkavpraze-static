85d491f1017ed0dd00a7ab19c08f48b1e69b6820
Vaclav Sistek <vsistek@fsfe.org>
2020-05-10 03:57:56 +0200

    deploy has lock file - DOS protection

--- src/apps/deploy.sh
+++ src/apps/deploy.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 export LANG=C.UTF-8
 
+LOCKFILE=/var/www/lektorkavpraze-static/lock/just-deployed
 echo "Content-Type: text/plain; charset=utf-8"
 echo
+[ `find "$LOCKFILE" -mmin +5 2>/dev/null` ] && rm $LOCKFILE
+[ -f LOCKFILE ] && echo "deployed within 5min, try later" && exit 0
 echo mkdir log
 mkdir -p /var/www/lektorkavpraze-static/log
+echo mkdir lock
+mkdir -p /var/www/lektorkavpraze-static/lock
 cd /var/www/lektorkavpraze-static
 echo git pull
 git pull
 echo src/util/build.sh
 /var/www/lektorkavpraze-static/src/util/build.sh
+echo refresh lock
+touch $LOCKFILE
+
